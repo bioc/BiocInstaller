7cdbfeae40b068958e79d3a1efa2ee1480e97476
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-06-20 19:57:09 +0000

    don't use R.version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_10/madman/Rpacks/BiocInstaller@66876 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utilities.R
+++ R/utilities.R
@@ -58,8 +58,10 @@
 .isDevel <-
     function ()
 {
-    isOdd <- (packageVersion("BiocInstaller")$minor %% 2L) == 1L
-    isOdd && (R.version$status == "" || R.version$status == "Patched")
+    minor <- as.numeric(R.Version()$minor)
+    if (R.Version()$major == "2" && (minor >= 16 && minor < 17))
+        return(TRUE)
+    (packageVersion("BiocInstaller")$minor %% 2L) == 1L
 }
 
 # bootstrap() should take care of unloading BiocInstaller 
